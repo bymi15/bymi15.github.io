eecd981b932c849df381fcb6b552522a479f1842
Brian Min <bymi15@yahoo.com>
2015-12-23 00:22:37 +0000

    added alot of new stuff

---
+++ js/bxslider.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('#slider1').bxSlider({
+      auto: true,
+      autoControls: false,
+      pause: 3500
+    });
+
+    $('#slider2').bxSlider({
+      auto: true,
+      autoControls: false,
+      pause: 4000
+    });
+
+    $('#slider3').bxSlider({
+      auto: true,
+      autoControls: false,
+      pause: 4000
+    });
+
+});
