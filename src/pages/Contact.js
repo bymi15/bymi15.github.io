f966a2140f38ab083750d1a6e2abb980fb0115ef
Brian Min <bymi15@yahoo.com>
2021-11-21 04:32:50 +0000

    refactor: minor ui improvements

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ import ContactForm from "../containers/ContactForm";
 
 const Contact = () => (
   <Animated animationIn="fadeIn" animationInDelay={50}>
-    <Container className="pad-top" style={{ height: "90vh" }}>
+    <Container className="pad-top" style={{ height: "calc(100vh - 85px)" }}>
       <div className="section-title">
         <h2>Contact</h2>
         <p>Feel free to get in touch with me for any enquiries</p>
