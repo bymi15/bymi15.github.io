d4173da4e7fcaae16fbe1886041580b01005669a
Brian Min <bymi15@yahoo.com>
2021-11-24 16:57:23 +0000

    fix: add public url prefix to images

--- src/utils/getImagePathArr.js
+++ src/utils/getImagePathArr.js
@@ -2,7 +2,7 @@ const getImagePathArr = (imageName, extension, count) => {
   let arr = [];
   let i;
   for (i = 1; i <= count; i++) {
-    arr.push("/img/" + imageName + i + "." + extension);
+    arr.push(process.env.PUBLIC_URL + "/img/" + imageName + i + "." + extension);
   }
   return arr;
 };
